a83dda4721c36f833abf74295ccb36b2e748a494
Sruthi Mavuleti <mavuleti160@gmail.com>
2025-01-08 14:45:38 -0800

    Corrected App.css

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -4,19 +4,19 @@ const Event = ({ event }) => {
     const [showDetails, setShowDetails] = useState(false);
 //console.log("EVENT:", event);
     return (
-        <li>
-            <div className="eventSummary">
+        <li className="event">
+            <div >
                 <h2>{event.summary}</h2>
                 <p>{event.location}</p>
                 <p>{event.created}</p>
             </div>
             {showDetails ? (
-                <div className="eventDetails">
+                <div>
                     <p>{event.description}</p>
                 </div>
             ) : null}
             <button 
-                className="show-details-btn"
+                className="details-btn"
                 onClick={() => setShowDetails(!showDetails)}
             >
                 {showDetails ? "Hide Details" : "Show Details"}
