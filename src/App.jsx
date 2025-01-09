9a50e53cd3dc67803d18fd0dacc754ac4c123c0c
Sruthi Mavuleti <mavuleti160@gmail.com>
2025-01-08 16:57:15 -0800

    Updated Number of events box

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react'; 
-import './App.css';
-import EventList from './components/EventList';
-import CitySearch from './components/CitySearch';
-import NumberOfEvents from './components/NumberOfEvents';
-import { extractLocations, getEvents } from './api';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import EventList from "./components/EventList";
+import CitySearch from "./components/CitySearch";
+import NumberOfEvents from "./components/NumberOfEvents";
+import { extractLocations, getEvents } from "./api";
 
 const App = () => {
   const [events, setEvents] = useState([]);
@@ -18,9 +18,10 @@ const App = () => {
 
   const fetchData = async () => {
     const allEvents = await getEvents();
-    const filteredEvents = currentCity === "See all cities"
-      ? allEvents
-      : allEvents.filter(event => event.location === currentCity);
+    const filteredEvents =
+      currentCity === "See all cities"
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
 
     setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
@@ -28,20 +29,25 @@ const App = () => {
 
   return (
     <div className="App">
-      <CitySearch 
-        allLocations={allLocations} 
-        setCurrentCity={setCurrentCity} 
-      />
-      <EventList 
-        events={events} 
-      />
-       <NumberOfEvents 
+      {/* Display the number of events on top */}
+      <h2 data-testid="event-count">
+        Displaying {events.length} of {currentNOE} events
+      </h2>
+
+      {/* NumberOfEvents Component */}
+      <NumberOfEvents
         setErrorAlert={setErrorAlert}
         currentNOE={currentNOE}
         setCurrentNOE={setCurrentNOE}
       />
+
+      {/* CitySearch Component */}
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+
+      {/* EventList Component */}
+      <EventList events={events} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/NumberOfEvents.jsx
+++ src/components/NumberOfEvents.jsx
@@ -1,35 +1,37 @@
-
 import React, { useState } from "react";
 
 const NumberOfEvents = ({ currentNOE, setCurrentNOE, setErrorAlert }) => {
-    const [number, setNumber] = useState(currentNOE);
-    
-    const handleInputChanged = (event) => {
-        const value = event.target.value;
-        setNumber(value)
-        if(isNaN(value) || value <= 0) {
-            setErrorAlert('Enter a valid number');
-        } else if (value > 32) {
-            setErrorAlert('Only maximum of 32 is allowed');
-        } else {
-            setErrorAlert('');
-            setCurrentNOE(value);
-        }
-    };
+  const [number, setNumber] = useState(currentNOE);
 
-    return (
-        <div id="number-of-events">
-            <label>
-                Number of Events:
-            <input 
-                type="text"
-                value={number}
-                onChange={handleInputChanged}
-                data-testid="numberOfEventsInput"
-            />
-            </label>
-        </div>
-    )
+  const handleInputChanged = (event) => {
+    const value = parseInt(event.target.value, 10); // Parse value as an integer
+    setNumber(value);
+
+    if (isNaN(value) || value <= 0) {
+      setErrorAlert("Enter a valid number greater than 0");
+    } else if (value > 32) {
+      setErrorAlert("Only a maximum of 32 events is allowed");
+    } else {
+      setErrorAlert(""); // Clear the error message
+      setCurrentNOE(value); // Update the number of events in the parent component
+    }
+  };
+
+  return (
+    <div id="number-of-events">
+      <label>
+        Number of Events:
+        <input
+          type="number" 
+          value={number}
+          onChange={handleInputChanged}
+          data-testid="numberOfEventsInput"
+          min="1" 
+          max="32" 
+        />
+      </label>
+    </div>
+  );
 };
 
 export default NumberOfEvents;
