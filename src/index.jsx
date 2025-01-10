98648749375c0b4e8d2c43ae8c1c062a2ed2e496
Sruthi Mavuleti <mavuleti160@gmail.com>
2025-01-10 09:11:54 -0800

    Atatus’ integration Setup Trail

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as atatus from 'atatus-spa';
+
+atatus.config('3ddf482769aa4c478fbcea82502a68a4').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+atatus.notify(new Error('Test Atatus Setup'));
